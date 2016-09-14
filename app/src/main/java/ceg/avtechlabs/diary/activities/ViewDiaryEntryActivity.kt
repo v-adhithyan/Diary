14ec62b175be14aa2b76ed4eebca6fdff421cb61
v.adhithyan <aavispeaks@gmail.com>
2016-09-14 18:39:10 +0530

    cards animate while shown in view

---
+++ app/src/main/java/ceg/avtechlabs/diary/activities/ViewDiaryEntryActivity.kt
@@ -0,0 +1,14 @@
+package ceg.avtechlabs.diary.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ceg.avtechlabs.diary.R
+
+
+class ViewDiaryEntryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_diary_entry)
+    }
+}
--- app/src/main/java/ceg/avtechlabs/diary/adapters/EntryAdapter.kt
+++ app/src/main/java/ceg/avtechlabs/diary/adapters/EntryAdapter.kt
@@ -1,9 +1,11 @@
 package ceg.avtechlabs.diary.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.TextView
 
 import ceg.avtechlabs.diary.R
@@ -23,13 +25,26 @@ class EntryAdapter(private val entryList: List<DiaryEntry>) : RecyclerView.Adapt
     override fun onBindViewHolder(entryViewHolder: EntryViewHolder, i: Int) {
         val diaryEntry = entryList[i]
         entryViewHolder.dateTextView.text = diaryEntry.date
-        entryViewHolder.entryTextView.text = diaryEntry.entry
+
+        if(diaryEntry.entry.length > 140) {
+            entryViewHolder.entryTextView.text = diaryEntry.entry.substring(0, 138) + ".."
+        } else
+            entryViewHolder.entryTextView.text = diaryEntry.entry
+
+        entryViewHolder.entryTextView.tag = diaryEntry.entry
+        setAnimation(entryViewHolder.itemView, i)
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): EntryViewHolder {
         val itemView = LayoutInflater.from(viewGroup.context).inflate(R.layout.diary_entry_item, viewGroup, false)
 
         return EntryViewHolder(itemView)
+    }
+
+    private fun setAnimation(view: View, position: Int) {
+        val animation = AnimationUtils.loadAnimation(view.context, android.R.anim.slide_in_left)
+        //val animation = AnimationUtils.loadAnimation(view.context, R.anim.card_anim)
+        view.startAnimation(animation)
     }
 
     class EntryViewHolder(v: View) : RecyclerView.ViewHolder(v) {
@@ -41,6 +56,13 @@ class EntryAdapter(private val entryList: List<DiaryEntry>) : RecyclerView.Adapt
             dateTextView = v.findViewById(R.id.entry_card_item_date) as TextView
             entryTextView = v.findViewById(R.id.entry_card_item_entry) as TextView
             entryTextView.setQuiveraFont(v.context)
+
+            v.setOnClickListener { Log.v("test", entryTextView.tag.toString()) }
+            v.setOnCreateContextMenuListener { contextMenu, view, contextMenuInfo ->
+                contextMenu.setHeaderTitle("Action")
+                contextMenu.add("View entry")
+                contextMenu.add("Edit entry")
+            }
         }
     }
 
